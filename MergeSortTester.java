05ee5b63cdbe914c670d4b7f88d81314b104e942
Rihui Zheng <rzheng4@stuy.edu>
2017-02-14 20:16:02 -0800

    OMG SO LONG

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -13,7 +13,7 @@
   nlog(n)
   
   Mean execution times for dataset of size n:
-  Batch size: <# of times each dataset size was run>
+  Batch size: <1000>
   n=1       time: 
   n=10      time: 
   n=100     time: 
@@ -37,45 +37,128 @@ public class MergeSortTester
     {
 	MergeSort bob = new MergeSort();
 
-	int[] arr0 = new int[10];
-	for(int i = 0;i < 10; i++){
+	int[] arr0 = new int[1];
+	for(int i = 0;i < 1; i++){
 	    arr0[i] = (int)(Math.random()*1000);
 	}
-
-	int[] arr1 = new int[100];
-	for(int i = 0;i < 100; i++){
+	
+	int[] arr1 = new int[10];
+	for(int i = 0;i < 10; i++){
 	    arr1[i] = (int)(Math.random()*1000);
 	}
 
-	int[] arr2 = new int[1000];
-	for(int i = 0;i < 1000; i++){
+	int[] arr2 = new int[100];
+	for(int i = 0;i < 100; i++){
 	    arr2[i] = (int)(Math.random()*1000);
 	}
+
+	int[] arr3 = new int[1000];
+	for(int i = 0;i < 1000; i++){
+	    arr3[i] = (int)(Math.random()*1000);
+	}
+
+	int[] arr4 = new int[10000];
+	for(int i = 0;i < 10000; i++){
+	    arr4[i] = (int)(Math.random()*1000);
+	}
+
+	int[] arr5 = new int[100000];
+	for(int i = 0;i < 100000; i++){
+	    arr5[i] = (int)(Math.random()*1000);
+	}
+
+	int[] arr6 = new int[1000000];
+	for(int i = 0;i < 1000000; i++){
+	    arr6[i] = (int)(Math.random()*1000);
+	}
+	
 	System.out.println("Merge Sort:");
-	System.out.println("\t10 element array:");
-	for (int x = 0; x < 10; x++){
+	long average = 0;
+
+	average = 0;
+	for (int x = 0; x < 1000; x++){
 	    long start = System.nanoTime();
 	    bob.sort(arr0);
 	    long end = System.nanoTime();
 	    long elapsed = end - start;
-	    System.out.println("\t\t" + elapsed);
+	    average += elapsed;
+	    //System.out.println(elapsed);
 	    }
-	System.out.println("\t100 element array:");
-	for (int x = 0; x < 10; x++){
+	average = average / 1000;
+	System.out.println("1 element: \t\t" + average);
+
+	average = 0;
+	for (int x = 0; x < 1000; x++){
 	    long start = System.nanoTime();
 	    bob.sort(arr1);
 	    long end = System.nanoTime();
 	    long elapsed = end - start;
-	    System.out.println("\t\t" + elapsed);
+	    average += elapsed;
+	    //System.out.println(elapsed);
 	    }
-	System.out.println("\t1000 element array:");
-	for (int x = 0; x < 10; x++){
+	average = average / 1000;
+	System.out.println("10 elements: \t\t" + average);
+
+	average = 0;
+	for (int x = 0; x < 1000; x++){
 	    long start = System.nanoTime();
 	    bob.sort(arr2);
 	    long end = System.nanoTime();
 	    long elapsed = end - start;
-	    System.out.println("\t\t" + elapsed);
+	    average += elapsed;
+	    //System.out.println(elapsed);
 	    }
+	average = average / 1000;
+	System.out.println("100 elements: \t\t" + average);
+
+	average = 0;
+	for (int x = 0; x < 1000; x++){
+	    long start = System.nanoTime();
+	    bob.sort(arr3);
+	    long end = System.nanoTime();
+	    long elapsed = end - start;
+	    average += elapsed;
+	    //System.out.println(elapsed);
+	    }
+	average = average / 1000;
+	System.out.println("1,000 elements: \t\t" + average);
+
+	average = 0;
+	for (int x = 0; x < 1000; x++){
+	    long start = System.nanoTime();
+	    bob.sort(arr4);
+	    long end = System.nanoTime();
+	    long elapsed = end - start;
+	    average += elapsed;
+	    //System.out.println(elapsed);
+	    }
+	average = average / 1000;
+	System.out.println("10,000 elements: \t\t" + average);
+
+	average = 0;
+	for (int x = 0; x < 1000; x++){
+	    long start = System.nanoTime();
+	    bob.sort(arr5);
+	    long end = System.nanoTime();
+	    long elapsed = end - start;
+	    average += elapsed;
+	    //System.out.println(elapsed);
+	    }
+	average = average / 1000;
+	System.out.println("100,000 elements: \t\t" + average);
+
+	average = 0;
+	for (int x = 0; x < 1000; x++){
+	    long start = System.nanoTime();
+	    bob.sort(arr6);
+	    long end = System.nanoTime();
+	    long elapsed = end - start;
+	    average += elapsed;
+	    //System.out.println(elapsed);
+	    }
+	average = average / 1000;
+	System.out.println("1,000,000 elements: \t\t" + average);
+	
     }//end main
 
 }//end class
